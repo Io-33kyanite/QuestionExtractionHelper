56d843c1ae9d302e2f4061b6d405174d6fe77c1d
Sasaki, I <57740535+33ky-sasaki@users.noreply.github.com>
2021-12-15 16:27:57 +0900

    Initial Commit

---
+++ QuestionExtractionHelperUITests/QuestionExtractionHelperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QuestionExtractionHelperUITestsLaunchTests.swift
+//  QuestionExtractionHelperUITests
+//
+//  Created by SASAKI Iori on 2021/12/15.
+//
+
+import XCTest
+
+class QuestionExtractionHelperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
